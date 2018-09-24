bfdf9a95e698b02f049b1b38b1800190b8c1e370
Charan Teja <charan.teja@cashapona.com>
2018-09-24 21:59:48 +0530

    worked on containers

---
+++ lib/Containers.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new application());
+}
+
+class application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new Container(
+          child: new Text("This is my container"),
+          height: 300.0,
+          width: 300.0,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20.0),
+          decoration: new BoxDecoration(color: Colors.green),
+          foregroundDecoration: new BoxDecoration(color: Colors.deepOrange),
+        ),
+      ),
+    );
+  }
+}
